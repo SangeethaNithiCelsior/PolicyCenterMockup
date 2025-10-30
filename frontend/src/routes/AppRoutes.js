b9bbc52d0d727f311f0252612c26b43c2695172a
N Sangeetha <Sangeethan@pyramidconsultinginc.com>
2025-10-30 17:11:48 +0530

    update index.js

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/Login/LoginPage';
 import DesktopPage from '../pages/Desktop/DesktopPage';
 import CreateAccountPage from '../pages/CreateAccount/CreateAccountPage';
 import NewSubmissionsPage from '../pages/NewSubmissions/NewSubmissions';
 import ResetPasswordPage from '../pages/ResetPassword/ResetPassword';
 
-
 const AppRoutes = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/desktop" element={<DesktopPage />} />
-                <Route path="/reset-password" element={<ResetPasswordPage />} />
-                <Route path="/create-account" element={<CreateAccountPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/new-submission" element={<NewSubmissionsPage />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/desktop" element={<DesktopPage />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} />
+            <Route path="/create-account" element={<CreateAccountPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/new-submission" element={<NewSubmissionsPage />} />
+        </Routes>
     );
 };
 
