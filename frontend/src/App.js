4e8d3c4549df3906c4da1d6fd453b2d93b3a545e
N Sangeetha <Sangeethan@pyramidconsultinginc.com>
2025-11-19 13:15:55 +0530

    401 html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import './App.css';
 
 const App = () => {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter basename="/PolicyCenterMockup">
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
-
 export default App;
